0c72a704f465cf19dd353d1370a9f6317c6cfa37
shubham897 <56882368+shubham897@users.noreply.github.com>
2021-07-20 09:10:31 +0530

    first commit

---
+++ target/classes/sql/h2.sql
@@ -0,0 +1,33 @@
+ALTER TABLE POST
+  ALTER COLUMN create_date SET DEFAULT CURRENT_TIMESTAMP;
+
+-- Users
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES
+  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Name', 'Surname',
+   1);
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES
+  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'johndoe', 'John', 'Doe', 1);
+-- password in plaintext: "password"
+INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'ana', 'Ana', 'Surname', 1);
+
+-- Roles
+INSERT INTO ROLE (role_id, role)
+VALUES (1, 'ROLE_ADMIN');
+INSERT INTO ROLE (role_id, role)
+VALUES (2, 'ROLE_USER');
+
+-- User Roles
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (2, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (3, 2);
+
